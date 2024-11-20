f066b0bfb7a46a4dbd9c3f23f4b69af0f97038cb
leaderemran <leaderemran@gmail.com>
2024-11-20 19:22:18 +0600

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,31 @@
+var number = 96;
+
+var name = "Imran"
+
+var price = 99;
+
+
+var isRinging = false;
+
+
+var grilFriendName = "Alia vat"
+var PEI = 3.1416
+
+
+var leaderFriend = "kal nagini"
+leaderFriend = "kew na"
+
+
+var productPrice = 99.99;
+rpoductPrice = 100;
+
+
+var banana = 10;
+banana += 2
+
+var onionPrice
+
+console.log(rpoductPrice);
+
+
+
